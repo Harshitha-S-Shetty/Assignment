c7a14b2298c5d50b06a62a60d10041b0060ce5b5
Harshitha-S-Shetty <97578545+Harshitha-S-Shetty@users.noreply.github.com>
2024-07-30 18:07:41 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,23 +1,39 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import Header from './components/Header';
 import Content from './components/Content';
 import Login from './Pages/Login';
+import Home from './Pages/Home'
+import Product from './Pages/Product'
+import Pricing from './Pages/Pricing'
+import AboutUS from './Pages/AboutUS'
 
 import './index.css';
 
 function App() {
   return (
     <Router>
-      <Header />
-      <div className="min-h-screen flex flex-col">
-        <Content />
-      
-        <Routes>
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </div>
+      <RouteHandler />
     </Router>
+  );
+}
+
+function RouteHandler() {
+  const location = useLocation();
+  const isLoginPage = location.pathname === '/login';
+
+  return (
+    <>
+      {!isLoginPage && <Header />}
+      <Routes>
+        <Route path="/" element={<Content />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/product" element={<Product/>} />
+        <Route path="/pricing" element={<Pricing/>} />
+        <Route path="/aboutus" element={<AboutUS />} />
+      </Routes>
+    </>
   );
 }
 
