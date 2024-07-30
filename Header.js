c141d2c04486f0f4e045df27d25e81bf1362011b
Harshitha-S-Shetty <97578545+Harshitha-S-Shetty@users.noreply.github.com>
2024-07-30 14:25:52 +0530

    Update Header.js

--- Header.js
+++ Header.js
@@ -3,29 +3,31 @@ import { Link } from 'react-router-dom';
 
 function Header() {
   return (
-    <header className="bg-white shadow-md p-4">
-      <div className="container mx-auto flex justify-between items-center">
+    <header className="bg-white p-4 w-full min-h-[80px] flex items-center">
+      <div className="flex justify-between items-center w-full">
         <div className="flex items-center space-x-3 text-lg font-bold text-blue-800">
           <img
             src='https://media.licdn.com/dms/image/D560BAQFtI8XQ_-t63Q/company-logo_200_200/0/1711176610734/serproconsulting_logo?e=2147483647&v=beta&t=8YZysHswd8H3JYD6jOiqRE8hHxfvOTEWaKsQiq2o25c'
             alt='SerPro Consulting'
-            className='h-10 ml-10'
+            className='h-10'
           />
-          <span className="ml-1">SerPro<br/>Consulting</span>
+          <span className="ml-1">SerPro<br />Consulting</span>
         </div>
-        <nav className='flex grow ml-20'>
-          <ul className="flex space-x-7 items-center">
-            <li><a href="#home" className="text-gray-700">Home</a></li>
-            <li><a href="#product" className="text-gray-700">Product</a></li>
-            <li><a href="#pricing" className="text-gray-700">Pricing</a></li>
-            <li><a href="#about" className="text-gray-700">About Us</a></li>
-          </ul>
-        </nav>
-        <div>
-          <Link to="/register">
-            <button className="bg-gray-100 text-blue-800 border rounded-full px-5 py-2">Login</button>
-          </Link>
-          <button className="ml-6 mr-20 bg-blue-600 text-white rounded-full px-5 py-2">Request Demo</button>
+        <div className="flex flex-grow items-center justify-between ml-8">
+          <nav>
+            <ul className="flex space-x-5 items-center">
+              <li><a href="#home" className="text-gray-700">Home</a></li>
+              <li><a href="#product" className="text-gray-700">Product</a></li>
+              <li><a href="#pricing" className="text-gray-700">Pricing</a></li>
+              <li><a href="#about" className="text-gray-700">About Us</a></li>
+            </ul>
+          </nav>
+          <div className="flex items-center space-x-3">
+            <Link to="/register">
+              <button className="bg-gray-100 text-blue-800 border rounded-full px-4 py-1">Login</button>
+            </Link>
+            <button className="bg-blue-600 text-white rounded-full px-4 py-1">Request Demo</button>
+          </div>
         </div>
       </div>
     </header>
