a50a196f010f903ae67e35d450672756c4b03846
Harshitha-S-Shetty <97578545+Harshitha-S-Shetty@users.noreply.github.com>
2024-07-30 15:06:39 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
-import Register from './Pages/Register';
 import Content from './components/Content';
+import Login from './Pages/Login';
+
 import './index.css';
 
 function App() {
@@ -11,8 +12,9 @@ function App() {
       <Header />
       <div className="min-h-screen flex flex-col">
         <Content />
+      
         <Routes>
-          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </div>
     </Router>
